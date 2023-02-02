914f3e27d36acdeebc3f53234459c319eff5590a
Pyae Phyo Kyaw <pyaephyokyaw.work@gmail.com>
2023-02-02 21:20:41 +0900

    test: isANumber function

---
+++ tests/isNumber.test.ts
@@ -0,0 +1,26 @@
+import isNumber from "../src/helpers/isNumber"
+describe("is a Number Test", () => {
+    test("empty string", () => {
+        expect(isNumber("")).toBe(false)
+    })
+
+    test("a string", () => {
+        expect(isNumber("xx")).toBe(false)
+    })
+
+    test("string and number mix", () => {
+        expect(isNumber("xx11")).toBe(false)
+    })
+
+    test("a float string", () => {
+        expect(isNumber("10.11")).toBe(true)
+    })
+
+    test("a zero string", () => {
+        expect(isNumber("0")).toBe(true)
+    })
+
+    test("a number string", () => {
+        expect(isNumber("1994")).toBe(true)
+    })
+})
